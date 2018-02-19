0e7f6e5c75191a9b61d227f178f15551b7c00cf3
Naoki Shinagawa <naoki.shina@gmail.com>
2018-02-19 19:30:26 +0900

    complete model user test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,43 @@ describe User do
       user.valid?
       expect(user.errors[:email]).to include("can't be blank")
     end
+    it "is valid with a nickname, email, password, password_confirmation" do
+      user = build(:user)
+      expect(user).to be_valid
+    end
+    it "is invalid without a password" do
+      user = build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+    it "is invalid without a password_confirmation although with a password" do
+      user = build(:user, password_confirmation: "")
+      user.valid?
+      expect(user.errors[:password_confirmation]).to include("doesn't match Password")
+    end
+    it "is invalid over the 7 characters with nickname" do
+      user = build(:user, nickname: "aaaaaaa")
+      user.valid?
+      expect(user.errors[:nickname][0]).to include("is too long (maximum is 6 characters)")
+    end
+    it "is valid less than 6 characters with nickname" do
+      user = build(:user, nickname: "aaaaaa")
+      expect(user).to be_valid
+    end
+    it "is invalid with a duplicate email address" do
+      user = create(:user)
+      another_user = build(:user)
+      another_user.valid?
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+    it "is valid over the 8 characters with password" do
+      user = build(:user, password: "00000000")
+      expect(user).to be_valid
+    end
+    it "is invalid less than 5 characters with password" do
+      user = build(:user, password: "00000")
+      user.valid?
+      expect(user.errors[:password][0]).to include("is too short")
+    end
   end
 end
